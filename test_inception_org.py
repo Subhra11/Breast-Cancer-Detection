bed3e6f5ef09ea7de813797a050e4ae118a862f1
Subhra Mohanty <80302004+Subhra11@users.noreply.github.com>
2021-05-28 20:44:06 +1000

    Add files via upload
    test original images

---
+++ test_inception_org.py
@@ -0,0 +1,83 @@
+from tensorflow.python.keras import backend as K
+from tensorflow.python.keras.models import Model
+from tensorflow.python.keras.layers import Flatten, Dense, Dropout
+from tensorflow.python.keras.applications.inception_resnet_v2 import InceptionResNetV2, preprocess_input
+from keras.optimizers import Adam
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+
+
+DATASET_PATH  = '/fred/oz138/COS80028/P5/Subhra/Breast_cancer/Data/INbreast_png'
+IMAGE_SIZE    = (1024, 1024)
+NUM_CLASSES   = 2
+BATCH_SIZE    = 16  # try reducing batch size or freeze more layers if your GPU runs out of memory
+FREEZE_LAYERS = 2  # freeze the first this many layers for training
+NUM_EPOCHS    = 50
+WEIGHTS_FINAL = '/fred/oz138/COS80028/P5/Subhra/Breast_cancer/models/model-inception_resnet_v2-test.h5'
+
+
+train_datagen = ImageDataGenerator(preprocessing_function=preprocess_input,
+                                   rotation_range=40,
+                                   width_shift_range=0.2,
+                                   height_shift_range=0.2,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   channel_shift_range=10,
+                                   horizontal_flip=True,
+                                   fill_mode='nearest')
+train_batches = train_datagen.flow_from_directory(DATASET_PATH + '/train',
+                                                  target_size=IMAGE_SIZE,
+                                                  interpolation='bicubic',
+                                                  class_mode='categorical',
+                                                  shuffle=True,
+                                                  batch_size=BATCH_SIZE)
+
+test_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
+test_batches = test_datagen.flow_from_directory(DATASET_PATH + '/test',
+                                                  target_size=IMAGE_SIZE,
+                                                  interpolation='bicubic',
+                                                  class_mode='categorical',
+                                                  shuffle=False,
+                                                  batch_size=BATCH_SIZE)
+
+# show class indices
+print('****************')
+for cls, idx in train_batches.class_indices.items():
+    print('Class #{} = {}'.format(idx, cls))
+print('****************')
+
+# build our classifier model based on pre-trained InceptionResNetV2:
+# 1. we don't include the top (fully connected) layers of InceptionResNetV2
+# 2. we add a DropOut layer followed by a Dense (fully connected)
+#    layer which generates softmax class score for each class
+# 3. we compile the final model using an Adam optimizer, with a
+#    low learning rate (since we are 'fine-tuning')
+net = InceptionResNetV2(include_top=False,
+                        weights='imagenet',
+                        input_tensor=None,
+                        input_shape=(IMAGE_SIZE[0],IMAGE_SIZE[1],3))
+x = net.output
+x = Flatten()(x)
+x = Dropout(0.5)(x)
+output_layer = Dense(NUM_CLASSES, activation='softmax', name='softmax')(x)
+net_final = Model(inputs=net.input, outputs=output_layer)
+for layer in net_final.layers[:FREEZE_LAYERS]:
+    layer.trainable = False
+for layer in net_final.layers[FREEZE_LAYERS:]:
+    layer.trainable = True
+net_final.compile(optimizer=Adam(lr=1e-5),
+                  loss='categorical_crossentropy', metrics=['accuracy'])
+print(net_final.summary())
+
+# train the model
+net_final.fit_generator(train_batches,
+                        steps_per_epoch = train_batches.samples // BATCH_SIZE,
+                        validation_data = train_batches,
+                        validation_steps = train_batches.samples // BATCH_SIZE,
+                        epochs = NUM_EPOCHS)
+
+# save trained weights
+net_final.save(WEIGHTS_FINAL)
+
+test_batches.reset()
+score = model.evaluate_generator(test_batches, (test_batches.samples + (batchSize-1)) // batchSize)
+print("For test data set(original); Loss: ",score[0]," Accuracy: ", score[1])
